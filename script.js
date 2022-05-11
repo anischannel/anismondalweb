6c862766f3ca12b2796505530bdaf11fcfca5e90
Anis1620 <73384400+anischannel@users.noreply.github.com>
2022-05-11 17:58:34 +0530

    glwakbnd

--- script.js
+++ script.js
@@ -82,35 +82,22 @@ $(document).ready(function(){
     });
 });
 
-document.onkeydown = function(e) {
-    if(event.keyCode == 123) {
+document.oncontextmenu = () => {
+    alert("Pls dont try to copy our codes")
     return false;
+
+}
+
+document.onkeydown = e => {
+    if(e.key == "F12"){
+        alert("Pls dont try to copy our codes")
+        return false
     }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-    return false;
+    if(e.ctrlKey && e.key == "u") {
+        alert("Pls dont try to copy our codes")
+        return false
     }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-    return false;
-    }
-    }
+}
 
   
     
