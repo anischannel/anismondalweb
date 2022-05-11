230012f7cbd799d6ff6708cd187b9c19a5b43daa
Anis Mondal <73384400+anischannel@users.noreply.github.com>
2022-05-11 16:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,7 @@ $(document).ready(function(){
         }
     });
     
-    $(document).bind("contextmenu",function(e) {
- e.preventDefault();
-});
-    $(document).keydown(function(e){
-    if(e.which === 123){
-       return false;
-    }
-});
-
+   
     
     
     
