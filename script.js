c0dc64671ea0444c836ca04dd7fb028756afa867
Anis Mondal <73384400+anischannel@users.noreply.github.com>
2022-05-11 17:14:02 +0530

    wda

--- script.js
+++ script.js
@@ -112,12 +112,5 @@ document.onkeydown = function(e) {
     }
     }
 
-    $(document).bind("contextmenu",function(e) {
-        e.preventDefault();
-       });
-       $(document).keydown(function(e){
-           if(e.which === 123){
-              return false;
-           }
-       });
+  
     
